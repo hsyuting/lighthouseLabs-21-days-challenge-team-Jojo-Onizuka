b4d8fd63eb56c8972eeda64b073431fb073307a8
Alexander Song <alex.ky.song@gmail.com>
2019-11-11 01:51:20 -0800

    challenge 11 edit

--- challenge11.js
+++ challenge11.js
@@ -2,11 +2,9 @@
 const busTimes = buses => {
     // Code here!
     let newObj = {};
-    let busVals = [];
     
     for (const bus in buses) {
-      busVals = Object.values(buses[bus]);
-      newObj[bus] = busVals[0] / busVals[1];
+      newObj[bus] = buses[bus].distance / buses[bus].speed;
     }
   
     return newObj;
